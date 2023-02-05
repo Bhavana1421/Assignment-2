3209c0cee927371e885bd058fd81901c949fb414
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:49:06 +0530

    Create replace.java

---
+++ src/replace.java
@@ -0,0 +1,7 @@
+public class replace {
+    public static void main(String[] args) {
+                String s1="Parul University";
+                String replaceString=s1.replace('a','e');
+                System.out.println(replaceString);
+    }
+}
