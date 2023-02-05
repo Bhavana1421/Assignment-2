346d653bef8f0161fb43ad80ff4aee1872d2d780
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:43:27 +0530

    Create lastindexof.java

---
+++ src/lastindexof.java
@@ -0,0 +1,7 @@
+public class lastindexof {
+    public static void main(String[] args) {
+                String str = "This is Bhavana Reddy";
+                int index = str.lastIndexOf('B',5);
+                System.out.println(index);
+    }
+}
