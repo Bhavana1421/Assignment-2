79723d2a7f0cd8d1a6189ba5d344a6f7c4d5ed56
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:14:00 +0530

    Create contains.java

---
+++ src/contains.java
@@ -0,0 +1,8 @@
+public class contains {
+    public static void main(String[] args) {
+                String str = "Hello parul students";
+                boolean isContains = str.contains("parul");
+                System.out.println(isContains);
+                System.out.println(str.contains("parul"));
+    }
+}
