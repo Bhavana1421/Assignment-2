4641a6f161c253aebe1bd20f986b1843493c977d
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:57:33 +0530

    Create substring.java

---
+++ src/substring.java
@@ -0,0 +1,11 @@
+public class substring {
+    public static void main(String[] args) {
+                String s1="Parul university";
+                String substr = s1.substring(0);
+                System.out.println(substr);
+                String substr2 = s1.substring(5,10);
+                System.out.println(substr2);
+                String substr3 = s1.substring(5,15);
+
+        }
+}
