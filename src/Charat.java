ecdf96dacc32bacba73a612543e41a6473b0e7f4
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 22:57:56 +0530

    Create Charat.java

---
+++ src/Charat.java
@@ -0,0 +1,9 @@
+public class Charat {
+    public static void main(String[] args) {
+
+                String name="Bhavana";
+                char ch=name.charAt(4);
+                System.out.println(ch);
+
+    }
+}
