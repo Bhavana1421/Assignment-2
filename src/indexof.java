b8151e993352d09ef0a05e9545a8012f1b023619
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:34:25 +0530

    Create indexof.java

---
+++ src/indexof.java
@@ -0,0 +1,8 @@
+public class indexof {
+    public static void main(String[] args) {
+                String s1 = "This is Bhavana Reddy";
+                // Passing Substring
+                int index = s1.indexOf("Reddy"); //Returns the index of this substring
+                System.out.println("is Bhavana "+index);
+    }
+}
