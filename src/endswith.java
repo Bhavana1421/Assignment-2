f7de71aefaf381e68dd2ccc2fc2d35e8f46ba8f8
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:17:32 +0530

    Create endswith.java

---
+++ src/endswith.java
@@ -0,0 +1,10 @@
+public class endswith {
+    public static void main(String[] args) {
+
+                String str = "hello Bhavana reddy";
+                System.out.println(str.endsWith("bhavana"));
+                if(str.endsWith("reddy")) {
+                    System.out.println("String ends with reddy");
+                }else System.out.println("It does not end with reddy");
+    }
+}
