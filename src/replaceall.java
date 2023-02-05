8dc328160a5de310fd8e502d640bfa924a233f70
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:50:51 +0530

    Create replaceall.java

---
+++ src/replaceall.java
@@ -0,0 +1,7 @@
+public class replaceall {
+    public static void main(String[] args) {
+                String s1="parul university is a private university";
+                String replaceString=s1.replaceAll("a","e");
+                System.out.println(replaceString);
+    }
+}
