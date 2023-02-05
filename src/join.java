7ff5501419b2a7dc35355ccc672fed0d420f7dc3
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:40:31 +0530

    Create join.java

---
+++ src/join.java
@@ -0,0 +1,7 @@
+public class join {
+    public static void main(String[] args) {
+                String str = null;
+                str = String.join(null, "abc", "bcd", "apple");
+                System.out.println(str);
+    }
+}
