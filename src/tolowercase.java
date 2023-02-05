47d9605b4a21d0f94709e7889b289fd0a14bab94
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-06 00:00:54 +0530

    Create tolowercase.java

---
+++ src/tolowercase.java
@@ -0,0 +1,7 @@
+public class tolowercase {
+    public static void main(String[] args) {
+                String s1="PARUL UNIVERSITY IS PRIVATE university";
+                String s1lower=s1.toLowerCase();
+                System.out.println(s1lower);
+    }
+}
