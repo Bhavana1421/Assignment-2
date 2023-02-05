96d5564b4e16715ba56378a3ae020d8e12283190
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:10:19 +0530

    Create concat.java

---
+++ src/concat.java
@@ -0,0 +1,14 @@
+public class concat {
+    public static void main(String[] args) {
+
+                String str1 = "Hello";
+                String str2 = "parul";
+                String str3 = "student";
+                // Concatenating one string
+                String str4 = str1.concat(str2);
+                System.out.println(str4);
+                // Concatenating multiple strings
+                String str5 = str1.concat(str2).concat(str3);
+                System.out.println(str5);
+    }
+}
