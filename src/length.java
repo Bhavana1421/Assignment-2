016f8011da6b3013f0967764cfe0b3ee99bd765d
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:46:05 +0530

    Create length.java

---
+++ src/length.java
@@ -0,0 +1,8 @@
+public class length {
+    public static void main(String[] args) {
+                String s1="Parul";
+                String s2="university";
+                System.out.println("string length is: "+s1.length());
+                System.out.println("string length is: "+s2.length());
+    }
+}
