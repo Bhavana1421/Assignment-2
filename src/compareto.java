0bf463f98800e350cff939ad461e7c3f4cebf805
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:05:52 +0530

    Create compareto.java

---
+++ src/compareto.java
@@ -0,0 +1,7 @@
+public class compareto {
+    public static void main(String[] args) {
+                String st1 = new String("I AM PARULITE");
+                String st2 = new String("i am parulite");
+                System.out.println(st1.compareTo(st2));
+            }
+        }
