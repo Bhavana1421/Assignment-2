2c58067bc7a3f28a9b10fa58e62881be50fe5c6d
Bhavana1421 <210303126204@paruluniversity.ac.in>
2023-02-05 23:55:30 +0530

    Create startswith.java

---
+++ src/startswith.java
@@ -0,0 +1,15 @@
+public class startswith {
+    public static void main(String[] args) {
+                String str = "Tokyo Olympics";
+
+                if(str.startsWith(""))
+                {
+                    System.out.println("The string starts with the empty string.");
+                }
+                else
+                {
+                    System.
+                            out.println("The string does not start with the empty string.");
+                }
+    }
+}
